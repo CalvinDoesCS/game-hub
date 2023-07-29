a919c2e662de16487d830e8741a3b233b357cf92
CalvinDoesCS <calvinntran.cs@gmail.com>
2023-07-29 01:24:36 -0400

    fix formatting

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -17,11 +17,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   if (error) return <Text>{error}</Text>;
 
   return (
-    <SimpleGrid
-      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-      padding="10px"
-      spacing={6}
-    >
+    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding="10px" spacing={6}>
       {isLoading &&
         skeletons.map((skeleton) => (
           <GameCardContainer key={skeleton}>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "f990da3bb0f34668986dbc1e81af28a0",
   },
 });
