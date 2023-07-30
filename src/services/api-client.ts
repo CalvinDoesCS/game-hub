4db44acb822984f9df9ba57f4acd6c355db4f623
CalvinDoesCS <calvinntran.cs@gmail.com>
2023-07-30 17:02:06 -0400

    Implement Infinite Queries

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,12 +1,29 @@
-import axios from "axios";
+import { useQuery } from "@chakra-ui/react";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
+  next: string | null;
   results: T[];
 }
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "f990da3bb0f34668986dbc1e81af28a0",
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string){
+    this.endpoint = endpoint;
+  }
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then(res=>res.data)
+  }
+}
+
+export default APIClient
