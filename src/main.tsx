bb56856218c21be674fbc2cc2a32d820215fe2cf
CalvinDoesCS <calvinntran.cs@gmail.com>
2023-08-18 00:53:21 -0400

    Refactor

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import App from "./App.tsx";
-import theme from "./theme.ts";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
-=======
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import {
   QueryClient,
@@ -40,7 +26,6 @@ ReactDOM.createRoot(
         <RouterProvider router={router} />
         <ReactQueryDevtools />
       </QueryClientProvider>
->>>>>>> game-hub-part2
     </ChakraProvider>
   </React.StrictMode>
 );
